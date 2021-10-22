869145a236cd11d858b76d309131a208d6a96ad5
Paul Morel <paul@paulmorel.com>
2021-10-21 22:43:09 -0400

    Add support for Kirby 3.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,24 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.0] - 2021-10-21
+
+### Added
+
+- Support for Kirby v3.6
+- `disableForRole` option that disables the tracking code snippet for specific user roles. Defaults to `[ 'admin' ]`
+- Tested support for lowest working Kirby 3 version and added badge at the top of readme file
+
+### Fixed
+
+- Loading icon not spinning while Fathom Analytics iframe is loading
+- Missing translation in dropdown menu
+
+### Changed
+
+- French translations from *Métriques* to *Statistiques*
+
+
 ## [0.0.2] - 2021-09-30
 
 ### Fixed
@@ -17,5 +35,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Panel view to display the Fathom Analytics dashboard directily in the panel
 - Snippet to embed the Fathom Analytics tracking code 
 
+[1.0.0]: https://github.com/PaulMorel/kirby3-fathom-analytics/releases/tag/v1.0.0
 [0.0.2]: https://github.com/PaulMorel/kirby3-fathom-analytics/releases/tag/v0.0.2
 [0.0.1]: https://github.com/PaulMorel/kirby3-fathom-analytics/releases/tag/v0.0.1
--- index.php
+++ index.php
@@ -1,10 +1,51 @@
 <?php
+
+use Kirby\Cms\App as Kirby;
+
+/**
+ * Kirby Fathom Analytics Plugin
+ * A Kirby 3 plugin for integrating the Fathom Analytics service to your site.
+ *
+ * @package   Kirby Fathom Analytics Plugin
+ * @author    Paul Morel <paul@paulmorel.com>
+ * @link      https://github.com/PaulMorel/kirby3-fathom-analytics
+ * @copyright Paul Morel
+ * @license   https://opensource.org/licenses/MIT
+ */
+
 Kirby::plugin('paulmorel/fathom-analytics', [
 	'options' => [
         'siteId' => '',
+        'disableForRole' => ['admin']
 	],
     'snippets' => [
         'fathom-analytics-embed' => __DIR__ . '/snippets/fathom-analytics-embed.php'
+    ],
+    'areas' => [
+        'analytics' => function ($kirby) {
+            return [
+                'label' => t('paulmorel.fathom-analytics.analytics'),
+                'link' => 'analytics',
+                'menu' => true,
+                'icon' => "chart",
+                'views' => [
+                    [
+                        'pattern' => 'analytics',
+                        'action' => function() {
+                            return [
+                                'component' => 'analytics',
+                                'props' => [
+                                    'config' => [
+                                        'siteId' => option('paulmorel.fathom-analytics.siteId'),
+                                        'sharePassword' => option('paulmorel.fathom-analytics.sharePassword') ? hash('sha256', option('paulmorel.fathom-analytics.sharePassword')) : ''
+                                    ]
+                                  ],
+                            ];
+                        },
+                    ]
+                ]
+            ];
+        }
     ],
     'api' => [
         'routes' => [
@@ -24,10 +65,12 @@ Kirby::plugin('paulmorel/fathom-analytics', [
 	'translations' => [
         'en' => [
             'paulmorel.fathom-analytics.analytics' => 'Analytics',
+            'view.analytics' => 'Analytics',
             'paulmorel.fathom-analytics.siteId-missing' => 'Your Fathom Analytics Site ID is missing from the config file.',
         ],
         'fr' => [
-            'paulmorel.fathom-analytics.analytics' => 'Métriques',
+            'paulmorel.fathom-analytics.analytics' => 'Statistiques',
+            'view.analytics' => 'Statistiques',
             'paulmorel.fathom-analytics.siteId-missing' => 'L\'identifiant Fathom Analytics de votre site est absent du fichier de configuration.',
         ]
     ]
